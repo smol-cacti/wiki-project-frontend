d06574450766e78592edb2999a517a8993313797
BrendaSO <91513871+BrendaSO@users.noreply.github.com>
2023-06-02 19:02:20 +0200

    delete obsolete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { AlbumComponent } from './album/album.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +13,6 @@ import { AlbumComponent } from './album/album.component';
     HeaderComponent,
     HomeComponent,
     LoginComponent,
-    AlbumComponent
   ],
   imports: [
     BrowserModule,
